3d684c6264aafeb8c6803a097f48ea89d41b693d
farhan687 <farhan687@gmail.com>
2016-12-12 07:58:44 +0530

    Remove old sensors

--- app/app.js
+++ app/app.js
@@ -5,13 +5,6 @@ import {RangeSlider} from './sensors/RangeSlider';
 import {TextField} from './sensors/TextField';
 import {DataSearch} from './sensors/DataSearch';
 import {SingleRange} from './sensors/SingleRange';
-import {AppbaseList} from './sensors/AppbaseList';
-import {AppbaseSlider} from './sensors/AppbaseSlider';
-import {AppbaseSearch} from './sensors/AppbaseSearch';
-import {AppbaseDistanceSensor} from './sensors/AppbaseDistanceSensor';
-import {AppbaseInputField} from './sensors/AppbaseInputField';
-import {AppbaseButtonGroup} from './sensors/AppbaseButtonGroup';
-import {AppbaseGoogleSearch} from './sensors/AppbaseGoogleSearch';
 import {MultiRange} from './sensors/MultiRange';
 import {ToggleButton} from './sensors/ToggleButton';
 // middleware
@@ -20,7 +13,6 @@ import {manager} from './middleware/ChannelManager';
 var helper = require('./middleware/helper.js');
 
 module.exports = {
-	AppbaseReactiveMap: AppbaseReactiveMap,
 	SingleList: SingleList,
 	MultiList: MultiList,
 	RangeSlider: RangeSlider,
@@ -29,13 +21,7 @@ module.exports = {
 	SingleRange: SingleRange,
 	MultiRange: MultiRange,
 	ToggleButton: ToggleButton,
-	AppbaseList: AppbaseList,
-	AppbaseSlider: AppbaseSlider,
-	AppbaseSearch: AppbaseSearch,
-	AppbaseDistanceSensor: AppbaseDistanceSensor,
-	AppbaseInputField: AppbaseInputField,
-	AppbaseButtonGroup: AppbaseButtonGroup,
-	AppbaseGoogleSearch: AppbaseGoogleSearch,
+	AppbaseReactiveMap: AppbaseReactiveMap,
 	AppbaseChannelManager: manager,
 	AppbaseSensorHelper: helper
 };
