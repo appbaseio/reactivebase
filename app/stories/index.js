c436f76ca210d8359124e795364e40b11aca812d
Deepak Grover <deepakgrover333@gmail.com>
2016-12-15 04:44:48 +0530

    add SingleList stories

--- app/stories/index.js
+++ app/stories/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
-import SingleListDemo from './SingleListDemo';
+import { storiesOf, addDecorator } from '@kadira/storybook';
+import { withKnobs, text, boolean, number } from '@kadira/storybook-addon-knobs';
+
 import { Appbase } from 'appbase-js';
+import SingleListDefault from './SingleList.stories';
 
 require ('../../dist/css/bootstrap.min.css');
 require ('../../dist/css/materialize.min.css');
 require ('../../dist/css/style.min.css');
 
 storiesOf('SingleList', module)
+	.addDecorator(withKnobs)
 	.add('Basic', () => (
-		<SingleListDemo />
+		<SingleListDefault showSearch={true} />
+	))
+	.add('Without Search', () => (
+		<SingleListDefault showSearch={false} />
+	))
+	.add('Default Selected', () => (
+		<SingleListDefault showSearch={true} defaultSelected='London' />
+	))
+	.add('Custom Sort', () => (
+		<SingleListDefault showSearch={true} defaultSelected='London' sortBy='asc' />
+	))
+	.add('Playground', () => (
+		<SingleListDefault
+			title={text('Title', 'My Cities')}
+			showSearch={boolean('Show Search', true)}
+			defaultSelected={text('Default Selected', 'London')}
+		/>
 	));
