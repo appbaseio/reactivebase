e30338837cb1fe1a2e4d23de92d44d0aa3a0597c
Deepak Grover <deepakgrover333@gmail.com>
2016-12-14 18:53:04 +0530

    setup storybook and add SingleListDemo

---
+++ app/stories/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import Button from './Button';
+import SingleListDemo from './SingleListDemo';
+import { Appbase } from 'appbase-js';
+
+require ('../../dist/css/bootstrap.min.css');
+require ('../../dist/css/materialize.min.css');
+require ('../../dist/css/style.min.css');
+
+storiesOf('SingleList', module)
+	.add('Basic', () => (
+		<SingleListDemo />
+	));
