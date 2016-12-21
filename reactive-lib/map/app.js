c163731ccc01095bfad70d7a8e2fc50116a51727
Deepak Grover <deepakgrover333@gmail.com>
2016-12-22 05:20:01 +0530

    clean up

--- reactive-lib/map/app.js
+++
@@ -1,8 +0,0 @@
-// actuators
-import {AppbaseMap} from './actuators/AppbaseMap';
-import {ListResult} from './actuators/ListResult';
-
-module.exports = {
-	AppbaseMap: AppbaseMap,
-	AppbaseListResult: ListResult
-};
