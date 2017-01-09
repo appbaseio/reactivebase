c408dd0231b5b1428b592f718fe4e338bebaa0a9
Deepak Grover <deepakgrover333@gmail.com>
2017-01-09 19:09:38 +0530

    update storybook options

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,15 @@
 import { configure } from '@kadira/storybook';
+import { setOptions } from '@kadira/storybook-addon-options';
+
+setOptions({
+	name: 'reactivebase',
+	url: 'https://github.com/appbaseio/reactivebase',
+	goFullScreen: false,
+	showLeftPanel: true,
+	showDownPanel: true,
+	showSearchBox: false,
+	downPanelInRight: false,
+});
 
 function loadStories() {
 	require('../app/stories');
