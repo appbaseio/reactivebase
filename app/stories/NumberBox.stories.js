b14f5710ed04f6ca5b91b649480dc7ef6e008d32
kaushik <kaushik.varanasi1@gmail.com>
2017-02-05 21:51:41 +0530

    indentation linting

--- app/stories/NumberBox.stories.js
+++ app/stories/NumberBox.stories.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { ReactiveBase, NumberBox, ResultList } from '../app.js';
-import { ResponsiveStory } from '../middleware/helper.js';
+import React, {Component} from 'react';
+import {ReactiveBase, NumberBox, ResultList} from '../app.js';
+import {ResponsiveStory} from '../middleware/helper.js';
 require('./list.css');
 
 export default class NumberBoxDefault extends Component {
@@ -15,18 +15,20 @@ export default class NumberBoxDefault extends Component {
 	}
 
 	nameQuery(value) {
-		if(value) {
+		if (value) {
 			return {
 				match: {
 					'cars.name': value
 				}
 			};
-		} else return null;
-	}
+		} else
+			return null;
+		}
 
 	onData(res) {
-		let result, combineData = res.currentData;
-		if(res.mode === 'historic') {
+		let result,
+			combineData = res.currentData;
+		if (res.mode === 'historic') {
 			combineData = res.currentData.concat(res.newData);
 		}
 		if (combineData) {
@@ -40,19 +42,28 @@ export default class NumberBoxDefault extends Component {
 
 	itemMarkup(marker, markerData) {
 		return (
-			<a className="full_row single-record single_record_for_clone"
-				href="#"
-				key={markerData._id}>
-				<div className="text-container full_row" style={{'paddingLeft': '10px'}}>
+			<a className="full_row single-record single_record_for_clone" href="#" key={markerData._id}>
+				<div className="text-container full_row" style={{
+					'paddingLeft': '10px'
+				}}>
 					<div className="text-head text-overflow full_row">
 						<span className="text-head-info text-overflow">
-							{marker.name ? marker.name : ''} - {marker.brand ? marker.brand : ''}
+							{marker.name
+								? marker.name
+								: ''}
+							- {marker.brand
+								? marker.brand
+								: ''}
 						</span>
-						<span className="text-head-city">{marker.brand ? marker.brand : ''}</span>
+						<span className="text-head-city">{marker.brand
+								? marker.brand
+								: ''}</span>
 					</div>
 					<div className="text-description text-overflow full_row">
 						<ul className="highlight_tags">
-							{marker.price ? `Priced at $${marker.price}` : 'Free Test Drive'}
+							{marker.price
+								? `Priced at $${marker.price}`
+								: 'Free Test Drive'}
 						</ul>
 					</div>
 				</div>
@@ -62,33 +73,19 @@ export default class NumberBoxDefault extends Component {
 
 	render() {
 		return (
-			<ReactiveBase
-				appname="car-store"
-				username="cf7QByt5e"
-				password="d2d60548-82a9-43cc-8b40-93cbbe75c34c"
-			>
+			<ReactiveBase appname="car-store" username="cf7QByt5e" password="d2d60548-82a9-43cc-8b40-93cbbe75c34c">
 				<div className="row">
 					<div className="col s6 col-xs-6">
-						<NumberBox
-							sensorId="CarModelSensor"
-							appbaseField={this.props.mapping.name}
-							title="NumberBox"
-							{...this.props}
-						/>
+						<NumberBox sensorId="CarModelSensor" appbaseField={this.props.mapping.name} title="NumberBox" {...this.props}/>
 					</div>
 
 					<div className="col-xs-6">
-						<ResultList
-							sensorId="SearchResult"
-							appbaseField={this.props.mapping.name}
-							title="Cars"
-							from={0}
-							size={20}
-							onData={this.onData}
-							depends={{
-								CarModelSensor: {"operation": "must", defaultQuery: this.NameQuery}
-							}}
-						/>
+						<ResultList sensorId="SearchResult" appbaseField={this.props.mapping.name} title="Cars" from={0} size={20} onData={this.onData} depends={{
+							CarModelSensor: {
+								"operation": "must",
+								defaultQuery: this.NameQuery
+							}
+						}}/>
 					</div>
 				</div>
 			</ReactiveBase>
