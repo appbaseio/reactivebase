f41ea3333773593d542730ef0fd1323ca5793442
farhan687 <farhan687@gmail.com>
2017-02-17 14:57:43 +0530

    Add MultiDropdownRange Test

---
+++ test/MultiDropdownRange/MultiDropdownRange.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {MultiDropdownRangeTest} from './MultiDropdownRange';
+import {expectedValues} from './config';
+
+describe('MultiDropdownRange test', () => {
+	var response = null;
+	beforeAll(() => {
+		return MultiDropdownRangeTest().then((res) => {
+			response = res;
+			return response;
+		}).catch((err) => err);
+	});
+
+	test('Response should exists', ()=> {
+		expect(response).toBeTruthy();
+		expect(response.res).toBeTruthy();
+	})
+
+	test('Query should match', () => {
+		expect(response.res.appliedQuery).toMatchObject(expectedValues.appliedQuery);
+	});
+
+	test('result length', () => {
+		expect(response.res.newData.length).toBe(expectedValues.resultLength);
+	});
+
+});
