5b56101c8449b9d20c443d9ba369e53d8def3fee
Siddharth Kothari <sids.aquarius@gmail.com>
2017-02-22 15:11:43 +0530

    adds initialLoader prop

--- app/stories/ReactiveList.stories.js
+++ app/stories/ReactiveList.stories.js
@@ -75,7 +75,7 @@ export default class ReactiveListDefault extends Component {
 	}
 
 	render() {
-		let placeholder = (<h6>Select city to see the results.</h6>);
+		let placeholder = (<h6>Select a city to see the results.</h6>);
 		return (
 			<ReactiveBase
 				app="meetup2"
@@ -111,7 +111,7 @@ export default class ReactiveListDefault extends Component {
 							title="Input Filter"
 							customQuery={this.cityQuery}
 							searchPlaceholder="Search City"
-							includeSelectAll={true}
+							initialLoader="Loading cities.."
 						/>
 					</div>
 				</div>
