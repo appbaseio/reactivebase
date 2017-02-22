7cee2595d7d94acb47c050bb14e51ff9f6628f58
Deepak Grover <deepakgrover333@gmail.com>
2017-02-22 20:25:03 +0530

    linting rules updates and fixes

--- app/actuators/ReactiveElement.js
+++ app/actuators/ReactiveElement.js
@@ -38,8 +38,19 @@ export default class ReactiveElement extends Component {
 		this.initialize();
 	}
 
-	initialize(executeChannel = false) {
-		this.createChannel(executeChannel);
+	componentWillUpdate() {
+		setTimeout(() => {
+			if (this.streamProp !== this.props.stream) {
+				this.streamProp = this.props.stream;
+				this.removeChannel();
+				this.initialize(true);
+			}
+			if (this.size !== this.props.size) {
+				this.size = this.props.size;
+				this.removeChannel();
+				this.initialize(true);
+			}
+		}, 300);
 	}
 
 	// stop streaming request and remove listener when component will unmount
@@ -47,19 +58,191 @@ export default class ReactiveElement extends Component {
 		this.removeChannel();
 	}
 
-	componentWillUpdate() {
-		setTimeout(() => {
-			if (this.streamProp != this.props.stream) {
-				this.streamProp = this.props.stream;
-				this.removeChannel();
-				this.initialize(true);
+	// tranform the raw data to marker data
+	setMarkersData(hits) {
+		if (hits) {
+			return hits;
+		}
+		return [];
+	}
+
+	// Create a channel which passes the react and receive results whenever react changes
+	createChannel(executeChannel = false) {
+		// Set the react - add self aggs query as well with react
+		const react = this.props.react ? this.props.react : {};
+		if (react && react.and && typeof react.and === "string") {
+			react.and = [react.and];
+		}
+		react.and.push("streamChanges");
+		if (this.sortObj) {
+			this.enableSort(react);
+		}
+		// create a channel and listen the changes
+		const channelObj = manager.create(this.context.appbaseRef, this.context.type, react, this.props.size, this.props.from, this.props.stream);
+		this.channelId = channelObj.channelId;
+
+		this.channelListener = channelObj.emitter.addListener(channelObj.channelId, (res) => {
+			// implementation to prevent initialize query issue if old query response is late then the newer query
+			// then we will consider the response of new query and prevent to apply changes for old query response.
+			// if queryStartTime of channel response is greater than the previous one only then apply changes
+			if (res.error && res.startTime > this.queryStartTime) {
+				this.setState({
+					queryStart: false,
+					showPlaceholder: false
+				});
+				if (this.props.onData) {
+					const modifiedData = helper.prepareResultData(res.data);
+					this.props.onData(modifiedData.res, modifiedData.err);
+				}
 			}
-			if (this.size != this.props.size) {
-				this.size = this.props.size;
-				this.removeChannel();
-				this.initialize(true);
+			if (res.appliedQuery) {
+				if (res.mode === "historic" && res.startTime > this.queryStartTime) {
+					const visibleNoResults = res.appliedQuery && res.data && !res.data.error ? (!(res.data.hits && res.data.hits.total)) : false;
+					const resultStats = {
+						resultFound: !!(res.appliedQuery && res.data && !res.data.error && res.data.hits && res.data.hits.total)
+					};
+					if (res.appliedQuery && res.data && !res.data.error) {
+						resultStats.total = res.data.hits.total;
+						resultStats.took = res.data.took;
+					}
+					this.setState({
+						queryStart: false,
+						visibleNoResults,
+						resultStats,
+						showPlaceholder: false
+					});
+					this.afterChannelResponse(res);
+				} else if (res.mode === "streaming") {
+					this.afterChannelResponse(res);
+					this.updateResultStats(res.data);
+				}
+			} else {
+				this.setState({
+					showPlaceholder: true
+				});
 			}
-		}, 300);
+		});
+		this.listenLoadingChannel(channelObj);
+		if (executeChannel) {
+			const obj = {
+				key: "streamChanges",
+				value: ""
+			};
+			helper.selectedSensor.set(obj, true);
+		}
+	}
+
+	updateResultStats(newData) {
+		const resultStats = this.state.resultStats;
+		resultStats.total = helper.updateStats(resultStats.total, newData);
+		this.setState({
+			resultStats
+		});
+	}
+
+	listenLoadingChannel(channelObj) {
+		this.loadListener = channelObj.emitter.addListener(`${channelObj.channelId}-query`, (res) => {
+			if (res.appliedQuery) {
+				this.setState({
+					queryStart: res.queryState
+				});
+			}
+		});
+	}
+
+	afterChannelResponse(res) {
+		const data = res.data;
+		let rawData,
+			markersData,
+			newData = [],
+			currentData = [];
+		this.streamFlag = false;
+		if (res.mode === "streaming") {
+			this.channelMethod = "streaming";
+			newData = data;
+			newData.stream = true;
+			currentData = this.state.currentData;
+			this.streamFlag = true;
+			markersData = this.setMarkersData(rawData);
+		} else if (res.mode === "historic") {
+			this.queryStartTime = res.startTime;
+			this.channelMethod = "historic";
+			newData = data.hits && data.hits.hits ? data.hits.hits : [];
+			const normalizeCurrentData = this.normalizeCurrentData(res, this.state.currentData, newData);
+			newData = normalizeCurrentData.newData;
+			currentData = normalizeCurrentData.currentData;
+		}
+		this.setState({
+			rawData,
+			newData,
+			currentData,
+			markersData,
+			isLoading: false
+		}, () => {
+			// Pass the historic or streaming data in index method
+			res.allMarkers = rawData;
+			let modifiedData = JSON.parse(JSON.stringify(res));
+			modifiedData.newData = this.state.newData;
+			modifiedData.currentData = this.state.currentData;
+			delete modifiedData.data;
+			modifiedData = helper.prepareResultData(modifiedData, res.data);
+			const generatedData = this.props.onData ? this.props.onData(modifiedData.res, modifiedData.err) : this.defaultonData(modifiedData.res, modifiedData.err);
+			this.setState({
+				resultMarkup: generatedData,
+				currentData: this.combineCurrentData(newData)
+			});
+		});
+	}
+
+	// normalize current data
+	normalizeCurrentData(res, rawData, newData) {
+		const appliedQuery = JSON.parse(JSON.stringify(res.appliedQuery));
+		const currentData = JSON.stringify(appliedQuery) === JSON.stringify(this.appliedQuery) ? (rawData || []) : [];
+		if (!currentData.length) {
+			this.appliedQuery = appliedQuery;
+		} else {
+			newData = newData.filter((newRecord) => {
+				let notExits = true;
+				currentData.forEach((oldRecord) => {
+					if (`${newRecord._id}-${newRecord._type}` === `${oldRecord._id}-${oldRecord._type}`) {
+						notExits = false;
+					}
+				});
+				return notExits;
+			});
+		}
+		return {
+			currentData,
+			newData
+		};
+	}
+
+	combineCurrentData(newData) {
+		if (_.isArray(newData)) {
+			return this.state.currentData.concat(newData);
+		}
+		return this.streamDataModify(this.state.currentData, newData);
+	}
+
+	// append stream boolean flag and also start time of stream
+	streamDataModify(rawData, data) {
+		if (data) {
+			data.stream = true;
+			data.streamStart = new Date();
+			if (data._deleted) {
+				const hits = rawData.filter(hit => hit._id !== data._id);
+				rawData = hits;
+			} else {
+				const hits = rawData.filter(hit => hit._id !== data._id);
+				rawData = hits;
+				rawData.unshift(data);
+			}
+		}
+		return rawData;
+	}
+
+	initialize(executeChannel = false) {
+		this.createChannel(executeChannel);
 	}
 
 	removeChannel() {
@@ -75,219 +258,12 @@ export default class ReactiveElement extends Component {
 		}
 	}
 
-	// Create a channel which passes the react and receive results whenever react changes
-	createChannel(executeChannel = false) {
-		// Set the react - add self aggs query as well with react
-		let react = this.props.react ? this.props.react : {};
-		if (react && react.and && typeof react.and === "string") {
-			react.and = [react.and];
-		}
-		react.and.push("streamChanges");
-		if (this.sortObj) {
-			this.enableSort(react);
-		}
-		// create a channel and listen the changes
-		let channelObj = manager.create(this.context.appbaseRef, this.context.type, react, this.props.size, this.props.from, this.props.stream);
-		this.channelId = channelObj.channelId;
-
-		this.channelListener = channelObj.emitter.addListener(channelObj.channelId, function(res) {
-			// implementation to prevent initialize query issue if old query response is late then the newer query
-			// then we will consider the response of new query and prevent to apply changes for old query response.
-			// if queryStartTime of channel response is greater than the previous one only then apply changes
-			if (res.error && res.startTime > this.queryStartTime) {
-				this.setState({
-					queryStart: false,
-					showPlaceholder: false
-				});
-				if (this.props.onData) {
-					let modifiedData = helper.prepareResultData(data);
-					this.props.onData(modifiedData.res, modifiedData.err);
-				}
-			}
-			if (res.appliedQuery) {
-				if (res.mode === "historic" && res.startTime > this.queryStartTime) {
-					let visibleNoResults = res.appliedQuery && res.data && !res.data.error ? (res.data.hits && res.data.hits.total ? false : true) : false;
-					let resultStats = {
-						resultFound: res.appliedQuery && res.data && !res.data.error && res.data.hits && res.data.hits.total ? true : false
-					};
-					if (res.appliedQuery && res.data && !res.data.error) {
-						resultStats.total = res.data.hits.total;
-						resultStats.took = res.data.took;
-					}
-					this.setState({
-						queryStart: false,
-						visibleNoResults: visibleNoResults,
-						resultStats: resultStats,
-						showPlaceholder: false
-					});
-					this.afterChannelResponse(res);
-				} else if (res.mode === "streaming") {
-					this.afterChannelResponse(res);
-					this.updateResultStats(res.data);
-				}
-			} else {
-				this.setState({
-					showPlaceholder: true
-				});
-			}
-		}.bind(this));
-		this.listenLoadingChannel(channelObj);
-		if (executeChannel) {
-			let obj = {
-				key: "streamChanges",
-				value: ""
-			};
-			helper.selectedSensor.set(obj, true);
-		}
-	}
-
-	updateResultStats(newData) {
-		let resultStats = this.state.resultStats;
-		resultStats.total = helper.updateStats(resultStats.total, newData);
-		this.setState({
-			resultStats: resultStats
-		});
-	}
-
-	listenLoadingChannel(channelObj) {
-		this.loadListener = channelObj.emitter.addListener(channelObj.channelId + "-query", function(res) {
-			if (res.appliedQuery) {
-				this.setState({
-					queryStart: res.queryState
-				});
-			}
-		}.bind(this));
-	}
-
-	afterChannelResponse(res) {
-		let data = res.data;
-		let rawData, markersData, newData = [],
-			currentData = [];
-		this.streamFlag = false;
-		if (res.mode === "streaming") {
-			this.channelMethod = "streaming";
-			newData = res.data;
-			newData.stream = true;
-			currentData = this.state.currentData;
-			this.streamFlag = true;
-			markersData = this.setMarkersData(rawData);
-		} else if (res.mode === "historic") {
-			this.queryStartTime = res.startTime;
-			this.channelMethod = "historic";
-			newData = res.data.hits && res.data.hits.hits ? res.data.hits.hits : [];
-			let normalizeCurrentData = this.normalizeCurrentData(res, this.state.currentData, newData);
-			newData = normalizeCurrentData.newData;
-			currentData = normalizeCurrentData.currentData;
-		}
-		this.setState({
-			rawData: rawData,
-			newData: newData,
-			currentData: currentData,
-			markersData: markersData,
-			isLoading: false
-		}, function() {
-			// Pass the historic or streaming data in index method
-			res.allMarkers = rawData;
-			let modifiedData = JSON.parse(JSON.stringify(res));
-			modifiedData.newData = this.state.newData;
-			modifiedData.currentData = this.state.currentData;
-			delete modifiedData.data;
-			modifiedData = helper.prepareResultData(modifiedData, res.data);
-			let generatedData = this.props.onData ? this.props.onData(modifiedData.res, modifiedData.err) : this.defaultonData(modifiedData.res, modifiedData.err);
-			this.setState({
-				resultMarkup: generatedData,
-				currentData: this.combineCurrentData(newData)
-			});
-			if (this.streamFlag) {
-				this.streamMarkerInterval();
-			}
-		}.bind(this));
-	}
-
-	// Check if stream data exists in markersData
-	// and if exists the call streamToNormal.
-	streamMarkerInterval() {
-		let markersData = this.state.markersData;
-		let isStreamData = markersData.filter((hit) => hit.stream && hit.streamStart);
-		if (isStreamData.length) {
-			this.isStreamDataExists = true;
-			setTimeout(() => this.streamToNormal(), this.props.streamActiveTime * 1000);
-		} else {
-			this.isStreamDataExists = false;
-		}
-	}
-
-	// normalize current data
-	normalizeCurrentData(res, rawData, newData) {
-		let appliedQuery = JSON.parse(JSON.stringify(res.appliedQuery));
-		let currentData = JSON.stringify(appliedQuery) === JSON.stringify(this.appliedQuery) ? (rawData ? rawData : []) : [];
-		if (!currentData.length) {
-			this.appliedQuery = appliedQuery;
-		} else {
-			newData = newData.filter((newRecord) => {
-				let notExits = true;
-				currentData.forEach((oldRecord) => {
-					if (newRecord._id + "-" + newRecord._type === oldRecord._id + "-" + oldRecord._type) {
-						notExits = false;
-					}
-				});
-				return notExits;
-			});
-		}
-		return {
-			currentData: currentData,
-			newData: newData
-		};
-	}
-
-	combineCurrentData(newData) {
-		if (_.isArray(newData)) {
-			return this.state.currentData.concat(newData);
-		} else {
-			return this.streamDataModify(this.state.currentData, newData);
-		}
-	}
-
-	// append stream boolean flag and also start time of stream
-	streamDataModify(rawData, data) {
-		if (data) {
-			data.stream = true;
-			data.streamStart = new Date();
-			if (data._deleted) {
-				let hits = rawData.filter((hit) => {
-					return hit._id !== data._id;
-				});
-				rawData = hits;
-			} else {
-				let prevData = rawData.filter((hit) => {
-					return hit._id === data._id;
-				});
-				let hits = rawData.filter((hit) => {
-					return hit._id !== data._id;
-				});
-				rawData = hits;
-				rawData.unshift(data);
-			}
-		}
-		return rawData;
-	}
-
-	// tranform the raw data to marker data
-	setMarkersData(hits) {
-		let self = this;
-		if (hits) {
-			return hits;
-		} else {
-			return [];
-		}
-	}
-
 	// default markup
 	defaultonData(res, err) {
 		let result = null;
 		if (res && res.appliedQuery) {
 			result = (
-				<div className="row" style={{"marginTop": "60px"}}>
+				<div className="row" style={{ marginTop: "60px" }}>
 					<pre className="pull-left">
 						{JSON.stringify(res.newData, null, 4)}
 					</pre>
@@ -299,9 +275,8 @@ export default class ReactiveElement extends Component {
 
 	render() {
 		let title = null,
-			placeholder = null,
-			sortOptions = null;
-		let cx = classNames({
+			placeholder = null;
+		const cx = classNames({
 			"rbc-title-active": this.props.title,
 			"rbc-title-inactive": !this.props.title,
 			"rbc-placeholder-active": this.props.placeholder,
@@ -319,15 +294,15 @@ export default class ReactiveElement extends Component {
 
 		return (
 			<div className="rbc-reactiveelement-container">
-				<div ref="ListContainer" className={`rbc rbc-reactiveelement card thumbnail ${cx}`} style={this.props.componentStyle}>
+				<div className={`rbc rbc-reactiveelement card thumbnail ${cx}`} style={this.props.componentStyle}>
 					{title}
-					{this.props.resultStats && this.state.resultStats.resultFound ? (<ResultStats onResultStats={this.props.onResultStats} took={this.state.resultStats.took} total={this.state.resultStats.total}></ResultStats>) : null}
+					{this.props.resultStats && this.state.resultStats.resultFound ? (<ResultStats onResultStats={this.props.onResultStats} took={this.state.resultStats.took} total={this.state.resultStats.total} />) : null}
 					{this.state.resultMarkup}
 					{this.state.showPlaceholder ? placeholder : null}
 				</div >
-				{this.props.noResults && this.state.visibleNoResults ? (<NoResults defaultText={this.props.noResults.text}></NoResults>) : null}
-				{this.props.initialLoader && this.state.queryStart ? (<InitialLoader defaultText={this.props.initialLoader.text}></InitialLoader>) : null}
-				<PoweredBy></PoweredBy>
+				{this.props.noResults && this.state.visibleNoResults ? (<NoResults defaultText={this.props.noResults.text} />) : null}
+				{this.props.initialLoader && this.state.queryStart ? (<InitialLoader defaultText={this.props.initialLoader.text} />) : null}
+				<PoweredBy />
 			</div>
 		);
 	}
