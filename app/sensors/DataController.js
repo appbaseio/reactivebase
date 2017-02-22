0e0286b635626bfdeb8a02189ffd3419e46aa97e
Deepak Grover <deepakgrover333@gmail.com>
2017-02-22 21:37:03 +0530

    Fix linting in DataController

--- app/sensors/DataController.js
+++ app/sensors/DataController.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
-import { manager } from '../middleware/ChannelManager.js';
-var helper = require('../middleware/helper.js');
-import * as TYPES from '../middleware/constants.js';
+import React, { Component } from "react";
+import classNames from "classnames";
+import { manager } from "../middleware/ChannelManager";
+import * as TYPES from "../middleware/constants";
+
+const helper = require("../middleware/helper");
 
 export default class DataController extends Component {
-	constructor(props, context) {
+	constructor(props) {
 		super(props);
-		this.type = 'match';
+		this.type = "match";
 	}
 
 	// Set query information
@@ -18,38 +19,38 @@ export default class DataController extends Component {
 
 	// set the query type and input data
 	setQueryInfo() {
-		let valObj = {
+		const valObj = {
 			queryType: this.type,
 			inputData: this.props.appbaseField
 		};
 		if (this.props.customQuery) {
 			valObj.customQuery = this.props.customQuery;
 		}
-		let obj = {
+		const obj = {
 			key: this.props.componentId,
 			value: valObj
 		};
 		helper.selectedSensor.setSensorInfo(obj);
 	}
 
-	// use this only if want to create actuators
-	// Create a channel which passes the react and receive results whenever react changes
-	createChannel() {
-		if (this.props.react) {
-			let react = this.props.react ? this.props.react : {};
-			var channelObj = manager.create(this.context.appbaseRef, this.context.type, react);
-		}
-		setTimeout(this.setValue.bind(this), 100);
-	}
-
 	setValue() {
-		var obj = {
+		const obj = {
 			key: this.props.componentId,
 			value: this.props.value
 		};
 		// pass the selected sensor value with componentId as key,
-		let isExecuteQuery = true;
+		const isExecuteQuery = true;
 		helper.selectedSensor.set(obj, isExecuteQuery);
+	}
+
+	// use this only if want to create actuators
+	// Create a channel which passes the react and receive results whenever react changes
+	createChannel() {
+		if (this.props.react) {
+			const react = this.props.react ? this.props.react : {};
+			manager.create(this.context.appbaseRef, this.context.type, react);
+		}
+		setTimeout(this.setValue.bind(this), 100);
 	}
 
 	// render
@@ -63,16 +64,16 @@ export default class DataController extends Component {
 			dataLabel = (<span className="rbc-datalabel col s12 col-xs-12">{this.props.dataLabel}</span>);
 		}
 
-		let cx = classNames({
-			'rbc-title-active': this.props.title,
-			'rbc-title-inactive': !this.props.title,
-			'rbc-querylabel-active': this.props.dataLabel,
-			'rbc-querylabel-inactive': !this.props.dataLabel
+		const cx = classNames({
+			"rbc-title-active": this.props.title,
+			"rbc-title-inactive": !this.props.title,
+			"rbc-querylabel-active": this.props.dataLabel,
+			"rbc-querylabel-inactive": !this.props.dataLabel
 		});
 
 		return (
 			<div className={`rbc rbc-datacontroller card thumbnail ${cx}`}>
-			{
+				{
 				this.props.showUI ?
 				(
 					<div>
@@ -100,7 +101,7 @@ DataController.propTypes = {
 // Default props value
 DataController.defaultProps = {
 	showUI: false,
-	value: 'customValue'
+	value: "customValue"
 };
 
 // context type
