80958eeae89e0626db5c69b0e8ae36f108024c2e
farhan687 <farhan687@gmail.com>
2017-03-21 17:09:55 +0530

    Remove no results from datasearch story

--- app/stories/DataSearch.stories.js
+++ app/stories/DataSearch.stories.js
@@ -94,8 +94,6 @@ export default class DataSearchDefault extends Component {
 							react={{
 								"and": "VenueSensor"
 							}}
-							initialLoader="Loading results.."
-							noResults="No results found!"
 						/>
 					</div>
 				</div>
