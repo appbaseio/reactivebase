67273d0b05146c048e7a57e4ad247b206c548e3a
Divyanshu Maithani <div.blackcat@gmail.com>
2017-08-28 23:49:09 +0530

    Fix SingleDropdownRange filter updates for #188

--- app/sensors/SingleDropdownRange.js
+++ app/sensors/SingleDropdownRange.js
@@ -20,13 +20,19 @@ export default class SingleDropdownRange extends Component {
 
 	// Set query information
 	componentWillMount() {
-		this.setQueryInfo();
+		this.setQueryInfo(this.props);
 		this.checkDefault(this.props);
 		this.listenFilter();
 	}
 
 	componentWillReceiveProps(nextProps) {
-		this.checkDefault(nextProps);
+		if (this.props.defaultSelected !== nextProps.defaultSelected) {
+			this.checkDefault(nextProps);
+		}
+		if (this.props.showFilter !== nextProps.showFilter || this.props.filterLabel !== nextProps.filterLabel) {
+			this.setQueryInfo(nextProps);
+			this.handleChange(this.state.selected);
+		}
 	}
 
 	componentWillUnmount() {
@@ -64,18 +70,18 @@ export default class SingleDropdownRange extends Component {
 	}
 
 	// set the query type and input data
-	setQueryInfo() {
+	setQueryInfo(props) {
 		const obj = {
-			key: this.props.componentId,
+			key: props.componentId,
 			value: {
 				queryType: this.type,
-				inputData: this.props.appbaseField,
-				customQuery: this.props.customQuery ? this.props.customQuery : this.customQuery,
+				inputData: props.appbaseField,
+				customQuery: props.customQuery ? props.customQuery : this.customQuery,
 				reactiveId: this.context.reactiveId,
-				showFilter: this.props.showFilter,
-				filterLabel: this.props.filterLabel ? this.props.filterLabel : this.props.componentId,
+				showFilter: props.showFilter,
+				filterLabel: props.filterLabel ? props.filterLabel : props.componentId,
 				component: "SingleDropdownRange",
-				defaultSelected: this.urlParams !== null ? this.urlParams : this.props.defaultSelected
+				defaultSelected: this.urlParams !== null ? this.urlParams : props.defaultSelected
 			}
 		};
 		helper.selectedSensor.setSensorInfo(obj);
